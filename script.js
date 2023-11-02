689e081facc6451a6f6a8bb3d24524ec408704e0
iNalsur <107191821+iNalsur@users.noreply.github.com>
2023-11-02 18:02:50 +0200

    Added load eventlistener

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const div1 = document.querySelector(".div1");
-const div2 = document.querySelector("div2");
-const container = document.querySelector(".container");
+document.addEventListener("load", function() {
+    const div1 = document.querySelector(".div1");
+    const div2 = document.querySelector("div2");
+    const container = document.querySelector(".container");
 
-let currentDiv = div1;
-let nextDiv = div2;
+    let currentDiv = div1;
+    let nextDiv = div2;
 
-currentDiv.style.display = "block";
+    currentDiv.style.display = "block";
 
-setInterval(function() {
-    currentDiv.style.display = "none";
-    nextDiv.style.display = "block";
+    setInterval(function() {
+        currentDiv.style.display = "none";
+        nextDiv.style.display = "block";
 
-    const tempDiv = currentDiv;
-    currentDiv = nextDiv;
-    nextDiv = tempDiv;
-}, 10000);
+        const tempDiv = currentDiv;
+        currentDiv = nextDiv;
+        nextDiv = tempDiv;
+    }, 10000);
+});

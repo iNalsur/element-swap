50c2e9d74a414300433d1a2d13a0064d28b57bc7
iNalsur <107191821+iNalsur@users.noreply.github.com>
2023-11-02 18:12:43 +0200

    Changed eventlistener from load to DOMContentLoaded, because load didnt work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("load", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const div1 = document.querySelector(".div1");
     const div2 = document.querySelector(".div2");
     const container = document.querySelector(".container");

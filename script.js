680de271d8672a471178691572a18b9b7933b788
iNalsur <107191821+iNalsur@users.noreply.github.com>
2023-11-02 17:47:59 +0200

    Make to variables which makes div1 current and div2 next

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const div1 = document.querySelector(".div1");
 const div2 = document.querySelector("div2");
 const container = document.querySelector(".container");
+
+let currentDiv = div1;
+let nextDiv = div2;
